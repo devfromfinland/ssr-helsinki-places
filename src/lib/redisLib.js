507289cb0f899149e9288b43687db2285c631bb3
devfromfinland <contact@vietphan.info>
2021-02-28 10:49:47 +0200

    fix condition redis

--- src/lib/redisLib.js
+++ src/lib/redisLib.js
@@ -1,5 +1,4 @@
 import redis from 'redis'
-import request from 'request'
 
 const REDIS_PORT = 6379
 
@@ -7,7 +6,9 @@ export const redisClient = process.env.CI
   ? redis.createClient(REDIS_PORT, { host: 'redis' })
   : redis.createClient(REDIS_PORT)
 
-redisClient.set('serverAvailable', 'yes')
+redisClient.on('error', (err) => {
+  console.log('redis client error', err)
+})
 
 // close redisClient properly after all tests
 // ref: https://stackoverflow.com/a/54560610
@@ -20,17 +21,4 @@ export const closeRedisClient = async () => {
   // redis.quit() creates a thread to close the connection.
   // We wait until all threads have been run once to ensure the connection closes.
   await new Promise(resolve => setImmediate(resolve))
-}
-
-export const hasRedisServer = () => {
-  try {
-    redisClient.get('serverAvailable', (err, res) => {
-      if (err) return false
-      if (res === 'yes') return true
-      return false
-    })
-  } catch (error) {
-    console.log('redis server is not available')
-    return false
-  }
 }
--- src/routes/places.js
+++ src/routes/places.js
@@ -5,24 +5,39 @@ import fetch from 'node-fetch'
 import { DEFAULT_PAGE_SIZE, DEFAULT_LANGUAGE } from '../utils/helpers'
 import App from '../components/App'
 import Html from '../components/Html'
-import { redisClient, hasRedisServer } from '../lib/redisLib'
+import { redisClient } from '../lib/redisLib'
 
 const router = express.Router()
 
+const prepAndSendContent = (context, res) => {
+  const scripts = ['client.js']
+
+  const mainContent = ReactDOMServer.renderToString(<App {...context}/>)
+  const html = ReactDOMServer.renderToStaticMarkup(
+    <Html
+      children={mainContent}
+      scripts={scripts}
+      context={context}
+    />
+  )
+
+  res.send(`<!DOCTYPE html>${html}`)
+}
+
 router.get('/', async (req, res) => {
-  // TODO: check validity of page & size input
+  // TODO: additional check validity of page & size input
 
   const page = req.query.page ? parseInt(req.query.page, 10) : 1
   const size = req.query.size ? parseInt(req.query.size, 10) : DEFAULT_PAGE_SIZE
   const lang = req.query.lang ? req.query.lang : DEFAULT_LANGUAGE
   const redisKey = req.originalUrl
 
-  const scripts = ['client.js']
+  let places
+  let isFailed = false
+  let totalCount = 0
 
-  // check from cache first
-  if (hasRedisServer()) {
-    let totalCount = null
-
+  if (redisClient.connected) {
+    // check data from cache first
     redisClient.get('count', (err, count) => {
       if (err) throw err
       if (count) totalCount = parseInt(count, 10)
@@ -31,10 +46,8 @@ router.get('/', async (req, res) => {
     redisClient.get(redisKey, async (err, data) => {
       if (err) throw err
 
-      let places = null
-      let isFailed = false
-
       if (data) {
+        // cache expire after 1 day, so no need validate strategy
         places = JSON.parse(data)
       } else {
         try {
@@ -52,65 +65,40 @@ router.get('/', async (req, res) => {
           redisClient.setex(redisKey, 24 * 60 * 60, JSON.stringify(result.data))
         } catch (err) {
           isFailed = true
-          console.log(`error while fetching data: ${err}`)
-          // res.send(`<!doctype html><div id="root">Couldn't load data!</div>`)
+          console.log('API request failed')
         }
       }
 
-      const context = {
+      prepAndSendContent({
         places,
         page,
         size,
         totalCount,
         isFailed,
-      }
-
-      const mainContent = ReactDOMServer.renderToString(<App {...context}/>)
-      const html = ReactDOMServer.renderToStaticMarkup(
-        <Html
-          children={mainContent}
-          scripts={scripts}
-          context={context}
-        />
-      )
-
-      res.send(`<!DOCTYPE html>${html}`)
+      }, res)
     })
   } else {
-    // no redis server for caching
-    let isFailed = false
-    let places
-    let totalCount
-
+    // no connection to redis server, query as normal
     try {
       const response = await fetch(`http://open-api.myhelsinki.fi/v1/places/?limit=${size}&start=${(page-1)*size}&language_filter=${lang}`)
       const result = await response.json()
       places = result.data
-      totalCount = parseInt(result.meta.count)
+      totalCount = parseInt(result.meta.count, 10)
     } catch (err) {
       isFailed = true
       console.log('API request failed')
     }
-    
-    const context = {
+
+    prepAndSendContent({
       places,
       page,
       size,
       totalCount,
       isFailed,
-    }
-
-    const mainContent = ReactDOMServer.renderToString(<App {...context}/>)
-    const html = ReactDOMServer.renderToStaticMarkup(
-      <Html
-        children={mainContent}
-        scripts={scripts}
-        context={context}
-      />
-    )
-
-    res.send(`<!DOCTYPE html>${html}`)
+    }, res)
   }
+
+  
 })
 
 export default router
